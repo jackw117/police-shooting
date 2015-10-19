006024fd30fb4affc929a407c5de82c150fcda79
Jack Whitesell <jackw117@uw.edu>
2015-10-18 19:00:27 -0700

    first and possibly last commit

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Police shootings
 
 This repository is a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/police-shooting).
+
+Website link is here: http://students.washington.edu/jackw117/info343/police-shooting/
--- js/map.js
+++ js/map.js
@@ -1,35 +1,84 @@
-// Function to draw your map
+//This is the javascript file that creates the map for the Police Shooting page
+
+//creates the map using tiles from OpenStreetMap, then calls getData
 var drawMap = function() {
-
-  // Create map and set view
- 
-
-  // Create a tile layer variable using the appropriate url
-
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+  map = L.map('container').setView([38, -100], 5) //centers America on the map
+  var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
+  layer.addTo(map)
+  getData()
 }
 
-// Function for getting data
+//collects the data from data/response.json and calls customBuild after a success
 var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-
-
-  // When your request is successful, call your customBuild function
-
+  $.ajax({
+    url: "data/response.json",
+    type: "get",
+    success: function(data) {
+      getData = data,
+      customBuild(data);
+    },
+    dataType: "json"
+  });
 }
 
-// Loop through your data and add the appropriate layers and points
+//loops through the data and adds circles to the map in categories based on race, and
+//changes color based on whether the victim was hit or killed, then creates a table with
+//information on whether or not a victim was killed, and if they were armed
 var customBuild = function() {
-	// Be sure to add each layer to the map
-
-	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+  var names = [];
+  var killArmed = 0;
+  var killUnarmed = 0;
+  var hitArmed = 0;
+  var hitUnarmed = 0;
+  var layerNames = [];
+  for (var i = 0; i < getData.length; i++) {
+    var check = getData[i];
+    var num = 0;
+    if (check.hasOwnProperty('Race')) {
+      if (names.indexOf(check.Race) == -1) {
+        names.push(check.Race);
+        num = names.indexOf(check.Race);
+        layerNames[num] = new L.layerGroup([]);
+      }
+      num = names.indexOf(check.Race);
+    } else {
+        if (names.indexOf("Unknown") == -1) {
+          names.push("Unknown");
+          num = names.indexOf("Unknown");
+          layerNames[num] = new L.layerGroup([]);
+        }
+      num = names.indexOf("Unknown");
+    }
+    var circle = new L.circleMarker([check.lat, check.lng]);
+    circle.options.radius = 8;
+    circle.bindPopup(check.Summary);
+    if (check['Hit or Killed?'] == 'Killed') {
+      if (check['Armed or Unarmed?'] == 'Armed') {
+        killArmed++;
+      } else if (check['Armed or Unarmed?'] == 'Unarmed') {
+        killUnarmed++;
+      }
+      circle.options.fillColor = "black";
+      circle.options.color = "black";
+    } else if (check['Hit or Killed?'] == 'Hit') {
+      if (check['Armed or Unarmed?'] == 'Armed') {
+        hitArmed++;
+      } else if (check['Armed or Unarmed?'] == 'Unarmed') {
+        hitUnarmed++;
+      }
+      circle.options.fillColor = "red";
+      circle.options.color = "red";
+    }
+    circle.addTo(layerNames[num]);
+  }
+  var layers = {};
+  for (var i = 0; i < layerNames.length; i++) {
+    layerNames[i].addTo(map);
+    layers[names[i]] = layerNames[i];   
+  }
+  $('#hitArmed').prepend(hitArmed);
+  $('#killArmed').prepend(killArmed);
+  $('#hitUnarmed').prepend(hitUnarmed);
+  $('#killUnarmed').prepend(killUnarmed);
+  L.control.layers(null, layers).addTo(map); 
 }
-
-
